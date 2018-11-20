b4101cffa6405e4bbf09c7e230e2f3920fe306a6
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2018-11-20 16:04:49 +0000

    aula 31

--- aula31-b4-web-server-express/bundles-server.js
+++ aula31-b4-web-server-express/bundles-server.js
@@ -1,31 +1,13 @@
 'use strict'
 
 const http = require('http')
-const url = require('url')
+const express = require('express')
+const morgan = require('morgan')
 const bundlesWebApi = require('./bundles-web-api')
 
-const webServer = new WebServer()
+const webServer = express()
+webServer.use(morgan('dev'))
 bundlesWebApi(webServer)
 http
-    .createServer(webServer.router)
+    .createServer(webServer)
     .listen(3000, () => console.log('Server running on port 3000'))
-
-function WebServer() {
-    const routes = []
-    this.use = (r) => {
-        routes.push(r)
-    }
-    this.router = (req, resp) => {
-        const {pathname}  = url.parse(req.url, true) // true to parse also the query-string
-        const method = req.method
-        console.log(`${Date()}: ${method} request to ${pathname}`)
-        
-        for (let index = 0; index < routes.length; index++) {
-            const r = routes[index]
-            if(r(req, resp)) {
-                // The route sends a response
-                break
-            }
-        }
-    }
-}
--- aula31-b4-web-server-express/bundles-web-api.js
+++ aula31-b4-web-server-express/bundles-web-api.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const url = require('url')
 const Bundle = require('./lib/bundle')
 // const Bundle = require('./lib/bundle-mock')
 
@@ -14,58 +13,34 @@ const es = {
 const bundle = Bundle.init(es)
 
 module.exports = (app) => {
-    app.use(postBundle)
-    app.use(getBundle)
+    app.post('/api/bundle', postBundle)
+    app.get('/api/bundle/:id', getBundle)
     app.use(resourceNotFond)
-    return app
+    app.use(errorHandler)
 
-    function postBundle(req, resp) {
-        const {pathname, query}  = url.parse(req.url, true) // true to parse also the query-string
-        const method = req.method
-        console.log(`${Date()}: request to ${pathname}`)
-    
-        if(method == 'POST' && pathname == '/api/bundle'){
-            const name = query.name
-            bundle.create(name, (err, data) => {
-                if(err) {
-                    resp.statusCode = err.code
-                    resp.end()
-                } else {
-                    resp.statusCode = 200
-                    resp.end(JSON.stringify(data))
-                }
-            })
-            // create a bundle
-            return true
-        }
-        return false
+    function postBundle(req, resp, next) {
+        const name = req.query.name
+        bundle.create(name, (err, data) => {
+            if(err) return next(err)
+            resp.json(data)
+        })
     }
-
-    function getBundle(req, resp) {
-        const {pathname}  = url.parse(req.url, true) // true to parse also the query-string
-        const method = req.method
-        console.log(`${Date()}: request to ${pathname}`)
-    
-        if(method == 'GET' && pathname.indexOf('/api/bundle') >= 0){
-            const id = pathname.split('/').pop()
-            bundle.get(id, (err, bundle) => {
-                if(err) {
-                    resp.statusCode = err.code
-                    resp.end()
-                } else {
-                    resp.statusCode = 200
-                    resp.end(JSON.stringify(bundle))
-                }
-            })
-            return true
-        }
-        return false
+    function getBundle(req, resp, next) {
+        const id = req.params.id
+        bundle.get(id, (err, bundle) => {
+            if(err) return next(err)
+            resp.json(bundle)
+        })
     }
-
-    function resourceNotFond(req, resp) {
-        resp.statusCode = 404
-        resp.end('Resource Not Found!')
-        return true
+    function resourceNotFond(req, resp, next) {
+        next({
+            'code': 404,
+            'error': 'Resource Not Found!'
+        })
+    }
+    function errorHandler(err, req, res, next) {
+        res.statusCode = err.code
+        res.end(err.error)
     }
 }
 
