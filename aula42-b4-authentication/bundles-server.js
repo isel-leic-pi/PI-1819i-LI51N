f1530425190511cd806cfd8b5bf081c388316098
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2018-12-18 14:49:32 +0000

    Refactor bundle-server

--- aula42-b4-authentication/bundles-server.js
+++ aula42-b4-authentication/bundles-server.js
@@ -10,37 +10,34 @@ const webpackMiddleware = require('webpack-dev-middleware')
 const nconf = require('nconf')
 
 /**
- * Setup the express instance
+ * Read configurations
  */
 nconf
     .argv()
     .env()
     .defaults({'NODE_ENV': 'development'})
-
-const app = express()
-app.use(morgan('dev'))
-
 const NODE_ENV = nconf.get('NODE_ENV')
 const isDev = NODE_ENV == 'development'
-
 console.log('Running ' + NODE_ENV)
-
-if(isDev)
-    app.use(webpackMiddleware(webpack(webpackConfig), {
-        publicPath: '/',
-        stats: {colors: true}
-    }))
-else
-    app.use(express.static('dist'))
-
+/**
+ * Setup the express instance
+ */
+const app = express()
+app.use(morgan('dev'))
+app.use(frontEndMiddleware(isDev))
 /**
  * Add bundle routes
  */
 bundlesWebApi(app)
-
 /**
  * Starts the web server
  */
 http
     .createServer(app)
     .listen(3000, () => console.log('Server running on port 3000'))
+
+function frontEndMiddleware(isDev) {
+    return isDev
+        ? webpackMiddleware(webpack(webpackConfig))
+        : express.static('dist')
+}
